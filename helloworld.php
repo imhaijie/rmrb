9a91e380c23f4942307eff280736ef918484203a
imhaijie <40633683+imhaijie@users.noreply.github.com>
2018-06-30 18:39:34 +0800

    ok5

--- helloworld.php
+++ helloworld.php
@@ -1,14 +1,14 @@
 <?php
 
-echo 'Hello, World!';
+echo 'Hello, World!<br>';
 $g1=date("y-m")."/".date(d);
 $g2=date(ymd);
 for ($x=1; $x<=12; $x++) {
   	for($hh=1;$hh<=8;$hh++)
 	{
 		echo "第".$x."版 第".$hh."篇：<br>";
-		if($x<10)echo "http://paper.people.com.cn/rmrb/html/".$g1."/nw.D110000renmrb_20".date(ymd)."_".$hh."-0".$x."htm<br>";
-		else echo "http://paper.people.com.cn/rmrb/html/".$g1."/nw.D110000renmrb_20".date(ymd)."_".$hh."-".$x.".htm<br>";
+		if($x<10)echo "http://paper.people.com.cn/rmrb/html/20".$g1."/nw.D110000renmrb_20".date(ymd)."_".$hh."-0".$x."htm<br>";
+		else echo "http://paper.people.com.cn/rmrb/html/20".$g1."/nw.D110000renmrb_20".date(ymd)."_".$hh."-".$x.".htm<br>";
 	}
 	
 	//echo "数字是：$x <br>";
