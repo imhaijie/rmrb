7d724ffa2237c480252f5db48e4e66e7718a9311
imhaijie <40633683+imhaijie@users.noreply.github.com>
2018-06-30 21:28:45 +0800

    ok6

--- helloworld.php
+++ helloworld.php
@@ -1,15 +1,16 @@
-<?php
 
+<?php
+echo "<meta charset="utf-8">"
 echo 'Hello, World!<br>';
 $g1=date("y-m")."/".date(d);
 $g2=date(ymd);
 for ($x=1; $x<=12; $x++) {
   	for($hh=1;$hh<=8;$hh++)
 	{
-		echo "µÚ".$x."°æ µÚ".$hh."Æª£º<br>";
-		if($x<10)echo "http://paper.people.com.cn/rmrb/html/20".$g1."/nw.D110000renmrb_20".date(ymd)."_".$hh."-0".$x."htm<br>";
+		echo "ç¬¬".$x."ç‰ˆ ç¬¬".$hh."ç¯‡ï¼š<br>";
+		if($x<10)echo "http://paper.people.com.cn/rmrb/html/20".$g1."/nw.D110000renmrb_20".date(ymd)."_".$hh."-0".$x.".htm<br>";
 		else echo "http://paper.people.com.cn/rmrb/html/20".$g1."/nw.D110000renmrb_20".date(ymd)."_".$hh."-".$x.".htm<br>";
 	}
 	
-	//echo "Êý×ÖÊÇ£º$x <br>";
+	//echo "æ•°å­—æ˜¯ï¼š$x <br>";
 }
